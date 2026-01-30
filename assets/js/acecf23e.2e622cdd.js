2892efd7af80e83e7e347bc1dd090ce950844309
Somanos Sar <somanos@drumee.com>
2026-01-30 12:46:38 +0100

    Clean landing page

--- assets/js/acecf23e.2e622cdd.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkdrumee_docs=globalThis.webpackChunkdrumee_docs||[]).push([[1903],{1912(s){s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
