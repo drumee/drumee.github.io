3794e187f0c4ab58ffceece2cc28ea417c94b1c9
Somanos Sar <somanos@drumee.com>
2026-05-28 17:03:23 +0700

    Updated src

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -11,40 +11,40 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Fully standlone, sovereign and scalable infrastructure',
+    title: 'Drumee is a sovereign data infrastructure',
     Svg: require("@site/static/img/logo.svg").default,
     description: (
       <>
-        Drumee OS is a meta operating system designed to run web applications like
-        MacOs to rune programs.
+        <p>It is not a cloud storage tool. It is not a SaaS collaboration app. It is an OS-like system that transforms a file system into a collaborative, extensible workspace - one that you fully own and control.</p>
+        <p><b>One sentence:</b> Drumee is a self-hosted, OS-like platform that turns a file system into a collaborative workspace - with full data sovereignty built in at the infrastructure level.</p>
       </>
     ),
   },
-  {
-    title: 'Ready to use Docker Image, deployable in minutes',
-    Svg: require('@site/static/img/docker.svg').default,
-    description: (
-      <>
-        A few lines of settings and you Drumee OS is ready to use. Also available on 
-        bare metal with Debian.
-      </>
-    ),
-  },
-  {
-    title: 'Add tailored plugins to meet your own requirements',
-    Svg: require('@site/static/img/plugin.svg').default,
-    description: (
-      <>
-        No need to develop boilerplates like Identities managements or
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+  // {
+  //   title: 'Ready to use Docker Image, deployable in minutes',
+  //   Svg: require('@site/static/img/docker.svg').default,
+  //   description: (
+  //     <>
+  //       A few lines of settings and you Drumee OS is ready to use. Also available on 
+  //       bare metal with Debian.
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: 'Add tailored plugins to meet your own requirements',
+  //   Svg: require('@site/static/img/plugin.svg').default,
+  //   description: (
+  //     <>
+  //       No need to develop boilerplates like Identities managements or
+  //       be extended while reusing the same header and footer.
+  //     </>
+  //   ),
+  // },
 ];
 
 function Feature({title, Svg, description}: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx('col')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
